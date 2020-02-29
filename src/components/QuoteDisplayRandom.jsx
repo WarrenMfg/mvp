019e01295fa7751c4b6248b23ae35df1e4c5b4ee
WarrenMfg <warrenmfg@icloud.com>
2020-02-29 18:22:07 -0500

    rendering random quote

--- src/components/QuoteDisplayRandom.jsx
+++ src/components/QuoteDisplayRandom.jsx
@@ -3,11 +3,30 @@ import React from 'react';
 class QuoteDisplayRandom extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      random: ''
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      const random = this.props.quotes[Math.floor(Math.random() * this.props.quotes.length)];
+      this.setState({ random });
+    }
   }
 
   render() {
+    const { random } = this.state;
     return (
-      <div>QuoteDisplayRandom.jsx</div>
+      <div>
+        <p>{random.quote}</p>
+        <div>
+          <p>{random.author}</p>
+          <p>{random.contributor}</p>
+          <p>{random.dateAdded}</p>
+          {random.dateModified && <p>{random.dateModified}</p>}
+        </div>
+      </div>
     );
   }
 }
